e84895da832bd02001030398e14fb525a24c1f1b
Abhas Kumar <abhasroy@gmail.com>
2020-05-16 17:53:09 -0700

    Added some Kist Views

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List(posts) {
+                post in
+                Text(post.title)
+                } 
+            .navigationBarTitle("Hacker News")
+        }
     }
 }
 
@@ -19,3 +25,17 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "hello all"),
+    Post(id: "2", title: "hello you"),
+    Post(id: "3", title: "hello there"),
+    Post(id: "4", title: "hello me"),
+    Post(id: "5", title: "hello Joe"),
+    Post(id: "6", title: "hello Sheldon")
+]
